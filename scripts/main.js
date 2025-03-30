ee46e40a28b250d1cfd7790e097be919ec13338e
ZIIF <ziif616@gmail.com>
2025-03-30 21:07:33 +0300

    Release

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+const btn = document.getElementsByClassName('expand-btn')[0];
+const text = document.getElementsByClassName('main-text')[0];
+const arrow = document.querySelector('.arrow')
+let open = false;
+
+btn.addEventListener('click', (e) => {
+    const height = window.getComputedStyle(text).height
+    if (!open) {
+        text.classList.add('hidden');
+        text.classList.remove('show');
+        text.style.maxHeight = height;
+        arrow.style.transform = 'rotate(-180deg)';
+    } else {
+        text.classList.remove('hidden');
+        text.style.maxHeight = height;
+        text.classList.add('show')
+        arrow.style.transform = 'rotate(0deg)';
+    }
+    void text.offsetHeight;
+    open = !open;
+})
+
+// const statistic = document.querySelector('.statistic');
+//
+// function updateContent() {
+//     if (window.matchMedia('(max-width: 744px)').matches) {
+//         statistic.
+//     }
+// }
+//
+// updateContent();
+//
+// window.addEventListener('resize', updateContent);
